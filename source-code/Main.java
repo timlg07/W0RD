c12561912484c241f2efff820fc0f5413eef7fb9
T!M Greller <timlg07@gmail.com>
2018-11-10 23:27:57 +0100

    [only source-code] added export functionality

--- source-code/Main.java
+++ source-code/Main.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -62,6 +63,11 @@ public class Main extends Application {
 				saveUnder(primaryStage);
 			});
 			
+			Button exportAsHTML = new Button("Als HTML exportieren...");
+			exportAsHTML.setOnAction((ActionEvent e) -> {
+				export(primaryStage);
+			});
+			
 			Button loadFileButton = new Button("Öffnen...");
 			loadFileButton.setOnAction(new EventHandler<ActionEvent>(){
 				@Override
@@ -97,6 +103,7 @@ public class Main extends Application {
 			
 			tools.getItems().add(saveButton);
 			tools.getItems().add(saveUnderButton);
+			tools.getItems().add(exportAsHTML);
 			tools.getItems().add(loadFileButton);
 			tools.getItems().add(new Separator());
 			tools.getItems().add(new Label("Datei auswählen: "));
@@ -175,7 +182,45 @@ public class Main extends Application {
 		    } 
 		}
 	}
-    
+	
+	public void export(Window primaryStage) {
+		
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Wählen Sie Verzeichniss und Name");
+		fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Website","*.html"));
+		File file = fc.showSaveDialog(primaryStage); // TODO File Format
+		
+		new Thread( () -> {
+			setStateOut("exportieren...");
+			try {
+				Thread.sleep(1_000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			setStateOut((isRunning)? "bereit" : "Fehler");
+		}).start();
+		
+		PrintWriter pWriter = null; 
+		try { 
+		    pWriter = new PrintWriter(new BufferedWriter(new FileWriter(file))); 
+		    pWriter.println(toNormHTML(editor.getHtmlText()));
+		} catch (IOException ioe) { 
+		    ioe.printStackTrace(); 
+		} finally { 
+		    if (pWriter != null){ 
+		    	pWriter.flush(); 
+		        pWriter.close(); 
+		    } 
+		}
+	}
+	
+	private static String toNormHTML(String text) {
+		return text
+			.replaceFirst("<body contenteditable=\"true\">", "<body>")
+			.replaceFirst("</body>", "<div style=\"position:fixed;bottom:0;left:50%;transform:translate(-50%,0%);-webkit-transform:translate(-50%,0%);\"><font face=\"Source Code Pro Black\" size=\"2\" color=\"#f2f2f2\" style=\"background-color: rgb(51, 51, 51);\">&nbsp;Created with W0RD by Tim Greller&nbsp;</font></div></body>")
+		;
+	}
+	
 	private static String load(File file) { 
 
         if (!file.canRead() || !file.isFile()) 
